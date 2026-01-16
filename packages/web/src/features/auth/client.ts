d1afaad56c632d61b628239779f0d9ebe48d85c1
Balint Orosz <orosz.balint.97@gmail.com>
2026-01-16 23:49:36 +0100

    add VITE_VERCE_URL

--- packages/web/src/features/auth/client.ts
+++ packages/web/src/features/auth/client.ts
@@ -2,8 +2,8 @@ import { createAuthClient } from "better-auth/react";
 import { emailOTPClient, organizationClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
+  baseURL: import.meta.env.VITE_VERCEL_URL
+    ? `https://${import.meta.env.VITE_VERCEL_URL}`
     : "http://localhost:3000",
   plugins: [emailOTPClient(), organizationClient()],
 });
